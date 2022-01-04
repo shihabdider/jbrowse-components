e11444d2c448cda34daed9d45ddadab8561681dd
Shihab Dider <shihabdider@gmail.com>
2022-01-04 14:56:30 -0800

    Merging refactor. Deleted some old files and did general refactoring of typescript.

--- plugins/flashmap-old/src/FlashmapAdapter/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as BigsiHitsAdapterClass } from './BigsiHitsAdapter'
-export { default as MashmapHitsConfigAdapterClass } from './MashmapHitsAdapter'
-export { default as MashmapOutputAdapterClass } from './MashmapOutputAdapter'
-export {
-  BigsiHitsSchema,
-  MashmapHitsConfigSchema,
-  MashmapOutputSchema,
-} from './configSchema'
